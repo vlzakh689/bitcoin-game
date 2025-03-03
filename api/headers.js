fdd17785589ff2201a12d1fd184da28307188fdd
vlad <zakharov.vlad2009@gmail.com>
2025-03-03 17:38:10 +0100

    add CORS headers to API response

---
+++ api/headers.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+    res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.status(200).end();
+  }
+  
